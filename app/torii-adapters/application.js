e4de26c5f841ae7331ba61b9b3ce3d71e28333a1
Drew Chandler <drew@kongregate.com>
2017-04-13 13:05:12 -0700

    Remember sessions

--- app/torii-adapters/application.js
+++ app/torii-adapters/application.js
@@ -1,13 +1,28 @@
 import Ember from 'ember';
+import RSVP from 'rsvp';
+
+const SESSION_KEY = 'session';
 
 export default Ember.Object.extend({
   open({id, attributes: { username, 'api-key': apiKey } }) {
-    return Ember.Object.create({
-      apiKey,
-      currentUser: { id, username }
+    let session = { apiKey, currentUser: { id, username } };
+    window.localStorage.setItem(SESSION_KEY, JSON.stringify(session));
+    return Ember.Object.create(session);
+  },
+
+  fetch() {
+    return new RSVP.Promise((resolve, reject) => {
+      let rawSession = window.localStorage.getItem(SESSION_KEY);
+      if (!rawSession) {
+        reject();
+      }
+
+      let session = JSON.parse(rawSession);
+      resolve(Ember.Object.create(session));
     });
   },
 
   close() {
+    window.localStorage.removeItem(SESSION_KEY);
   }
 });
--- tests/helpers/module-for-acceptance.js
+++ tests/helpers/module-for-acceptance.js
@@ -16,6 +16,7 @@ export default function(name, options = {}) {
     },
 
     afterEach() {
+      window.localStorage.clear();
       let afterEach = options.afterEach && options.afterEach.apply(this, arguments);
       return Promise.resolve(afterEach).then(() => destroyApp(this.application));
     }
