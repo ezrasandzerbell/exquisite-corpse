1f0fcdb41180391d280e3290f070a6af4f755462
Drew Chandler <drew@kongregate.com>
2017-05-03 11:22:50 -0700

    Workaround ember-ajax/ember-data incompatibility

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,17 @@
 import DS from 'ember-data';
 import AjaxServiceSupport from 'ember-ajax/mixins/ajax-support';
+import { InvalidError as AjaxInvalidError } from 'ember-ajax/errors';
+import { InvalidError as DSInvalidError } from 'ember-data/adapters/errors';
 
 export default DS.JSONAPIAdapter.extend(AjaxServiceSupport, {
+  // https://github.com/ember-cli/ember-ajax/issues/224
+  ajax() {
+    return this._super(...arguments).catch((e) => {
+      if (e instanceof AjaxInvalidError) {
+        throw new DSInvalidError(e.errors);
+      }
+
+      throw e;
+    });
+  }
 });
